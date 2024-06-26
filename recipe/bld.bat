0f170908e921419b7af7053e18dfe336f5594efe
Jeongseok Lee <jeongseok@meta.com>
2024-06-26 08:58:17 -0700

    Run tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,20 @@
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% ^
-                           -DTF_BUILD_EXAMPLES=OFF ^
-                           -DTF_BUILD_TESTS=OFF ^
-                           -DTF_BUILD_BENCHMARKS=OFF ^
-                           -DTF_BUILD_CUDA=OFF ^
-                           -DTF_BUILD_SYCL=OFF
+@echo on
+
+cmake %SRC_DIR% ^
+  -B build ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% ^
+  -DTF_BUILD_EXAMPLES=OFF ^
+  -DTF_BUILD_TESTS=ON ^
+  -DTF_BUILD_BENCHMARKS=OFF ^
+  -DTF_BUILD_CUDA=OFF ^
+  -DTF_BUILD_SYCL=OFF
 if errorlevel 1 exit 1
 
-nmake
+cmake --build build --parallel --config Release
 if errorlevel 1 exit 1
 
-nmake install
+ctest --test-dir build --output-on-failure --build-config Release
+if errorlevel 1 exit 1
+
+cmake --build build --target install --config Release
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,21 @@
 set -e
 
 cmake $SRC_DIR \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DTF_BUILD_EXAMPLES=OFF \
-      -DTF_BUILD_TESTS=OFF \
-      -DTF_BUILD_BENCHMARKS=OFF \
-      -DTF_BUILD_CUDA=OFF \
-      -DTF_BUILD_SYCL=OFF
+  ${CMAKE_ARGS} \
+  -G Ninja \
+  -B build \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DTF_BUILD_EXAMPLES=OFF \
+  -DTF_BUILD_TESTS=ON \
+  -DTF_BUILD_BENCHMARKS=OFF \
+  -DTF_BUILD_CUDA=OFF \
+  -DTF_BUILD_SYCL=OFF
 
-make install
+cmake --build build --parallel
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  ctest --test-dir build --output-on-failure
+fi
+
+cmake --build build --target install
